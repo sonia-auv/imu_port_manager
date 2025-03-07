b5fd61be61746781ddef19b4d1f737ab4be10764
Nimai Jariwala <nimai.jariwala.1@ens.etsmtl.ca>
2025-03-06 20:45:55 -0500

    Added magnetometer extraction

--- src/ProviderIMU.cpp
+++ src/ProviderIMU.cpp
@@ -394,6 +394,15 @@ namespace provider_imu
                     msg.orientation.w = std::stof(parameter);
 
                     std::getline(ss, parameter, ',');
+                    //msg.magnetometer.x = std::stof(parameter);
+
+                    std::getline(ss, parameter, ',');
+                    //msg.magnetometer.y = std::stof(parameter);
+
+                    std::getline(ss, parameter, ',');
+                    //msg.magnetometer.z = std::stof(parameter);
+
+                    std::getline(ss, parameter, ',');
                     msg.linear_acceleration.x = std::stof(parameter);
 
                     std::getline(ss, parameter, ',');
