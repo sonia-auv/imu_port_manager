fc0a03682ba6601389f7ce6e0eefa0d06a179d0b
Clement <Bytecrackerr@github.com>
2026-01-16 14:49:33 -0500

    LOG-151: finished implementation

--- src/ImuProvider.cpp
+++ src/ImuProvider.cpp
@@ -22,6 +22,10 @@ namespace imu_port_manager
         //wall timer
         timerNodeStatus = this->create_wall_timer(500ms, std::bind(&ImuProvider::publishStatus, this));
 
+        _node_status.node_name = this->get_name();
+        _node_status.quality = sonia_common_ros2::msg::NodeStatus::Q_OK;
+        _node_status.state = sonia_common_ros2::msg::NodeStatus::STATE_INITIALIZING;
+
         // Subscribers
         dvl_subscriber = this->create_subscription<geometry_msgs::msg::Twist>("/proc_nav/dvl_velocity", 100, std::bind(&ImuProvider::dvl_velocity, this, _1));
         vpe_basic_control = this->create_subscription<std_msgs::msg::UInt8MultiArray>("/provider_imu/vpe_basic_control", 10, std::bind(&ImuProvider::vpe_basic_control_callback, this, _1));
@@ -60,6 +64,7 @@ namespace imu_port_manager
         if (res)
         {
             _rs485Connection.Flush();
+            _node_status.state = sonia_common_ros2::msg::NodeStatus::STATE_IDLE;
         }
         return res;
     }
@@ -99,7 +104,6 @@ namespace imu_port_manager
     }
 
     void ImuProvider::publishStatus(){
-        _node_status.node_name = this->get_name();
         _node_status.stamp = this->now();
         publisher_node_status->publish(_node_status);
     }
@@ -291,10 +295,11 @@ namespace imu_port_manager
 
     void ImuProvider::reader()
     {
-	// Delay for port opening
-	std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        // Delay for port opening
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
         char buffer[BUFFER_SIZE];
 
+        _node_status.state = sonia_common_ros2::msg::NodeStatus::STATE_RUNNING;
         while (!_reader_stop_thread)
         {
             do
@@ -436,11 +441,13 @@ namespace imu_port_manager
 
                     std::getline(ss, parameter, '*');
                     msg.angular_velocity.z = std::stof(parameter);
-                    publisher->publish(msg);
+
+                    publisher->publish(msg);   
                 }
             }
             catch(...)
             {
+                _node_status.quality = sonia_common_ros2::msg::NodeStatus::Q_DEGRADE;
                 BOOST_LOG_TRIVIAL(info)<<"IMU : Bad packet register 15";
             }   
         }
